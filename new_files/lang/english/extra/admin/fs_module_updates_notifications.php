99f95e12f0fa34c5649299e8576caeeaa3577485
php-schubser <d.bjelajac@hoqmee.net>
2020-09-30 12:20:55 +0200

    chore: change in wording

--- new_files/lang/english/extra/admin/fs_module_updates_notifications.php
+++ new_files/lang/english/extra/admin/fs_module_updates_notifications.php
@@ -2,6 +2,6 @@
 define('TEXT_SUBSCRIBED_FOR_UPDATES', 'Your account was successfully activated!');
 define('TEXT_NOT_SUBSCRIBED_FOR_UPDATES', 'Something went wrong - we couldn\'t activate your account for MMLC module updates.');
 define('TEXT_NEW_MODULES_AVAILABLE', 'New modules are available on MMLC!');
-define('TEXT_FOLLOWING_MODULES_ARE_AVAILABLE', 'Following modules are new on MMLC: %s');
+define('TEXT_FOLLOWING_MODULES_ARE_AVAILABLE', 'Following modules are new on MMLC: <br/>%s');
 define('TEXT_MODULE_UPDATES_AVAILABLE', 'Module updates are available on MMLC!');
-define('TEXT_FOLLOWING_MODULES_ARE_AVAILABLE_FOR_UPDATE', 'Following modules are available for update on MMLC: %s');
+define('TEXT_FOLLOWING_MODULES_ARE_AVAILABLE_FOR_UPDATE', 'Following modules are available for update on MMLC: <br/>%s');
